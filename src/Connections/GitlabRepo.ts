77f44af9c290e367e6d5c85606bf3a913adf0fa3
Half-Shot <will@half-shot.uk>
2022-10-18 00:06:36 +0100

    Check access level via a better API

--- src/Connections/GitlabRepo.ts
+++ src/Connections/GitlabRepo.ts
@@ -191,8 +191,7 @@ export class GitLabRepoConnection extends CommandConnection<GitLabRepoConnection
         let permissionLevel;
         let project;
         try {
-            project = await client.projects.get(validData.path);
-            permissionLevel = Math.max(project.permissions.group_access?.access_level || 0, project.permissions.project_access?.access_level || 0) as AccessLevel;
+            permissionLevel = await client.projects.getMyAccessLevel(validData.path);
         } catch (ex) {
             throw new ApiError("Could not determine if the user has access to this project, does the project exist?", ErrCode.ForbiddenUser);
         }
